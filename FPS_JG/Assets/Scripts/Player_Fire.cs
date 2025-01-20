646fd4707953e55569373b9cc1307b7812c98ea3
Rhojingoo <rhojingoo@naver.com>
2025-01-20 20:18:30 +0900

    뷸렛 이펙트 오브젝트 풀 사용중

--- FPS_JG/Assets/Scripts/Player_Fire.cs
+++ FPS_JG/Assets/Scripts/Player_Fire.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class Player_Fire : MonoBehaviour
 {
-
     public GameObject Fire_Position;
 
+    GameObject[] Bullet_ObjectPool;
+    public int Bullet_PoolSize = 10;
     public GameObject Bomb_Factory;
     public float Throw_Power = 15f;
 
-    public GameObject Bullet_Effect;
+    // --------- (B) 이펙트 풀 관련 -----------
+    public GameObject Bullet_Effect;     // 파티클 프리팹
+    public int Effect_PoolSize = 10;     // 풀 크기
+    GameObject[] Effect_ObjectPool;
     //ParticleSystem Ps;
 
     // Start is called before the first frame update
     void Start()
     {
-      //  Ps = Bullet_Effect.GetComponent<ParticleSystem>();
+        // (A) 총알(폭탄) 풀: 예시만 있고, 아직 안 쓰고 있는 상태
+        //Bullet_ObjectPool = new GameObject[Bullet_PoolSize];
+        //for (int i = 0; i < Bullet_PoolSize; i++)
+        //{
+        //    GameObject bullet = Instantiate(Bomb_Factory);
+        //    bullet.SetActive(false);
+        //    Bullet_ObjectPool[i] = bullet;
+        //}
+
+        // (B) 뷸렛이펙트 풀 생성
+        Effect_ObjectPool = new GameObject[Effect_PoolSize];
+        for (int i = 0; i < Effect_PoolSize; i++)
+        {
+            GameObject effectObj = Instantiate(Bullet_Effect);
+            effectObj.SetActive(false);
+            Effect_ObjectPool[i] = effectObj;
+
+            // 파티클이 재생 완료되면 자동으로 풀에 반환하기 위한 스크립트(아래에서 설명)
+            //effectObj.AddComponent<ParticleAutoDisable>();
+        }
+        //  Ps = Bullet_Effect.GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -24,41 +48,57 @@ public class Player_Fire : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            print("왼쪽 클릭한다");
             Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-
             RaycastHit hitInfo = new RaycastHit();
+
 
             if (Physics.Raycast(ray, out hitInfo))
             {
-                Bullet_Effect.transform.position = hitInfo.point;
-
-                GameObject effectObj = Instantiate(Bullet_Effect, hitInfo.point, Quaternion.identity);
-                ParticleSystem effectPs = effectObj.GetComponent<ParticleSystem>();
-                 
-
-                if (effectPs != null)
+                GameObject effectObj = GetEffectFromPool();
+                if (effectObj != null)
                 {
-                    //EulerAngles를 통째로 대입하는 방법
-                    //effectPs.transform.eulerAngles = new Vector3(0f, 90f, 0f);
-                    //Quaternion.Euler를 사용하여 대입
-                    //effectPs.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+                    // 위치/회전 설정
+                    effectObj.transform.position = hitInfo.point;
 
+                    // 원하는 회전 처리 (예: 카메라 방향 보정)
+                    effectObj.transform.LookAt(Camera.main.transform);
+                    effectObj.transform.Rotate(0f, -90f, 0f);
 
-                    //충돌된 물체의 법선벡터 회전(보통은 이렇게 하면 될듯)
-                    //effectPs.transform.forward = hitInfo.normal;
-
-                    //카메라를 바라보는 방향으로 회전(이미지 자체가 옆으로 돌아 있어서서 설정변경)
-                    effectPs.transform.LookAt(Camera.main.transform);
-                    effectPs.transform.Rotate(0f, -90f, 0f);
-
-                    effectPs.Play();
+                    // 활성화하여 파티클 재생
+                    effectObj.SetActive(true);
+                    ParticleSystem ps = effectObj.GetComponent<ParticleSystem>();
+                    if (ps != null)
+                        ps.Play();
                 }
 
-               // print("레이 확인 한다");
+
+
 
                 //Bullet_Effect.transform.position = hitInfo.point;
-                // Ps.Play();
+                //GameObject effectObj = Instantiate(Bullet_Effect, hitInfo.point, Quaternion.identity);
+                //ParticleSystem effectPs = effectObj.GetComponent<ParticleSystem>();               
+
+                //if (effectPs != null)
+                //{
+                //    //EulerAngles를 통째로 대입하는 방법
+                //    //effectPs.transform.eulerAngles = new Vector3(0f, 90f, 0f);
+                //    //Quaternion.Euler를 사용하여 대입
+                //    //effectPs.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+
+
+                //    //충돌된 물체의 법선벡터 회전(보통은 이렇게 하면 될듯)
+                //    //effectPs.transform.forward = hitInfo.normal;
+
+                //    //카메라를 바라보는 방향으로 회전(이미지 자체가 옆으로 돌아 있어서서 설정변경)
+                //    effectPs.transform.LookAt(Camera.main.transform);
+                //    effectPs.transform.Rotate(0f, -90f, 0f);
+
+                //    effectPs.Play();
+                //}
+
+                //// print("레이 확인 한다");
+                // //Bullet_Effect.transform.position = hitInfo.point;
+                // // Ps.Play();
             }
 
         }
@@ -72,4 +112,22 @@ public class Player_Fire : MonoBehaviour
             rb.AddForce(Camera.main.transform.forward * Throw_Power, ForceMode.Impulse);
         }
     }
+
+
+    GameObject GetEffectFromPool()
+    {
+        for (int i = 0; i < Effect_ObjectPool.Length; i++)
+        {
+            // 아직 사용 중이 아닌(비활성화된) 오브젝트 찾기
+            if (!Effect_ObjectPool[i].activeSelf)
+            {
+                return Effect_ObjectPool[i];
+            }
+        }
+        // 남아있는 이펙트가 없다면(풀이 꽉 찼다면),
+        // 필요에 따라 새로 Instantiate하거나, null을 리턴하는 등 정책 결정
+        // 여기서는 null 리턴
+        return null;
+    }
+
 }
